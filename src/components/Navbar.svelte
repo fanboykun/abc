0d4169c39d7c65e369b65f7494f9e1ef10eb0df3
fanboykun <irfanramadhan1812@gmail.com>
2023-05-03 03:46:26 +0700

    structuring components

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,66 @@
+<header class="pointer-events-none relative z-50 flex flex-col" style="height:var(--header-height);margin-bottom:var(--header-mb)">
+    <div class="top-0 z-10 h-16 pt-6" style="position:var(--header-position)">
+        <div
+            class="sm:px-8 top-[var(--header-top,theme(spacing.6))] w-full"
+            style="position:var(--header-inner-position)"
+        >
+            <div class="mx-auto max-w-7xl lg:px-8">
+                <div class="relative px-4 sm:px-8 lg:px-12">
+                    <div class="mx-auto max-w-2xl lg:max-w-5xl">
+                        <div class="relative flex gap-4">
+                            <div class="flex flex-1">
+                                <div
+                                    class="h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10"
+                                >
+                                    <a aria-label="Home" class="pointer-events-auto" href="/"
+                                        ><img
+                                            alt=""
+                                            fetchpriority="high"
+                                            width="512"
+                                            height="512"
+                                            decoding="async"
+                                            data-nimg="1"
+                                            class="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800 h-9 w-9"
+                                            sizes="2.25rem"
+                                            srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=16&amp;q=75 16w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=32&amp;q=75 32w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=48&amp;q=75 48w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=64&amp;q=75 64w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=96&amp;q=75 96w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=128&amp;q=75 128w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=256&amp;q=75 256w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=384&amp;q=75 384w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=640&amp;q=75 640w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=750&amp;q=75 750w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=828&amp;q=75 828w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=1080&amp;q=75 1080w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=1200&amp;q=75 1200w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=1920&amp;q=75 1920w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=2048&amp;q=75 2048w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=3840&amp;q=75 3840w"
+                                            src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=3840&amp;q=75"
+                                            style="color: transparent;"
+                                        /></a
+                                    >
+                                </div>
+                            </div>
+                            <div class="flex flex-1 justify-end md:justify-center">
+
+                                <nav class="pointer-events-auto  md:block">
+                                    <ul
+                                        class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+                                    >
+                                        <li>
+                                            <a
+                                                class="relative block px-3 py-2 transition text-teal-500 dark:text-teal-400"
+                                                href="/"
+                                                >Home<span
+                                                    class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"
+                                                /></a
+                                            >
+                                        </li>
+                                        <li>
+                                            <a
+                                                class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+                                                href="/about">About</a
+                                            >
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <div class="flex justify-end md:flex-1">
+                                <div class="pointer-events-auto">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Stack from "../components/Stack.svelte";
+	import Header from "../components/Header.svelte";
+	import Projects from "../components/Projects.svelte";
+</script>
+
+<Header />
+<Projects />
+<Stack />
